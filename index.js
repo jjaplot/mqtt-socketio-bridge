2e4129d73283d15fbff44f4759d05445cff9d4cf
jjaplot <62018438+jjaplot@users.noreply.github.com>
2020-03-11 11:44:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -134,6 +134,23 @@ app.post('/chart-data',function(req,res){
         res.write(json_data)
 })
 
+app.get('/countdown', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  })
+  countdown(res, 10)
+})
+
+function countdown(res, count) {
+  res.write("data: " + count + "\n\n")
+  if (count)
+    setTimeout(() => countdown(res, count-1), 1000)
+  else
+    res.end()
+}
+
 http.listen(port, function() {
         console.log("listening on "+port)
 })
