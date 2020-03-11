bcbc393746d6c479248f5ee92fe8e56b2d123f2d
jjaplot <62018438+jjaplot@users.noreply.github.com>
2020-03-11 21:33:15 +0100

    Update gauge.js

--- static_files/js/gauge.js
+++ static_files/js/gauge.js
@@ -29,9 +29,11 @@ gauge.maxValue = 300; // set max gauge value
 gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
 gauge.animationSpeed = 50; // set animation speed (32 is default value)
 gauge.set(200); // set actual value
+
 client.on('message', function(topic, payload) {
+        document.getElementById('data2').innerHTML = 100;
         count = count + 1 
-        pdata = 200-count
+        pdata = 100-count
         console.log("topic: "+topic)
         console.log("payload: "+payload)
         gauge.set(pdata); // set actual value
