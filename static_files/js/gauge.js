f32100ffe57686872b7cdd5127071429a17216c4
jjaplot <62018438+jjaplot@users.noreply.github.com>
2020-03-12 08:22:16 +0100

    Update gauge.js

--- static_files/js/gauge.js
+++ static_files/js/gauge.js
@@ -1,26 +1,19 @@
 var opts = {
-angle: 0, // The span of the gauge arc
-lineWidth: 0.2, // The line thickness
-radiusScale: 0.89, // Relative radius
-pointer: {
-length: 0.54, // // Relative to gauge radius
-strokeWidth: 0.053, // The thickness
-color: '#000000' // Fill color
-},
-limitMax: false,     // If false, max value increases automatically if value > maxValue
-limitMin: false,     // If true, the min value of the gauge will be fixed
-colorStart: '#6FADCF',   // Colors
-colorStop: '#8FC0DA',    // just experiment with them
-strokeColor: '#E0E0E0',  // to see which ones work best for you
-generateGradient: true,
+lines: 0.6,
+  angle: -0.2,
+  lineWidth: 0.4,
+  pointer: {
+    length: 0.15,
+    strokeWidth: 0.07,
+    color: '#909090  '
+  },
+
+  limitMax: 'true', 
+  percentColors: [[0.0, "#FF4B57" ], [0.40, "#EBEB3A"], [0.48, "#5ACF40"], [0.481, '#FFF'], [0.52, "#EBEB3A"], [0.60, "#FF4B57"]],
+  strokeColor: '#E0E0E0',
+  generateGradient: true,
 highDpiSupport: true,     // High resolution support
-staticZones: [
-{strokeStyle: "#00FF00", min: 0, max: 60}, // Red from 100 to 60
-{strokeStyle: "#0000FF", min: 60, max: 150}, // Yellow
-{strokeStyle: "#00FFFF", min: 150, max: 220}, // Green
-{strokeStyle: "#FFDD00", min: 220, max: 260}, // Yellow
-{strokeStyle: "#FF0000", min: 260, max: 300}  // Red
-],
+
 };
 var target = document.getElementById('foo'); // your canvas element
 var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
